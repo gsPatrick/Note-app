5973d7342a2120581b9dc838202eee8cd2c456b5
unknown <patrickgsiqueira@hotmail.com>
2024-09-06 11:44:40 -0300

    NoteList.css and NoteList.tsx

---
+++ src/Greeting.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+interface GreetingProps{
+    name: string;
+}
+
+const Greeting: React.FC<GreetingProps> = ({name}) => {
+    return <h2>Hello, {name}!</h2>
+}
+
+
+export default Greeting;
